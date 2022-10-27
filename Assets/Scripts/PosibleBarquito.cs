3781246f3d8e237bb805505aac85dee8d0b3a6d6
CarlosPedretti <carlosluca150@gmail.com>
2022-10-26 21:49:42 -0300

    21:48 / 26/10
    Intento de disparo
    Cajita de municion en proceso
    Mejorando mar, etc...

--- Assets/Scripts/PosibleBarquito.cs
+++ Assets/Scripts/PosibleBarquito.cs
@@ -9,9 +9,16 @@ public class PosibleBarquito : MonoBehaviour
 	float movementFactor;
 	float horizontalInput;
 	float steerFactor;
+	float clampedSpeed;
+	float clampedSteerSpeed;
 	public float speed = 1.0f;
 	public float steerSpeed = 1.0f;
-	public float movementThresold = 10.0f;
+    public float movementThresold = 10.0f;
+	public float minSpeedLimit = 1f;
+	public float maxSpeedLimit = 3f;
+	public float minSpeedSteerLimit = 2f;
+	public float maxSpeedSteerLimit = 3f;
+
 
 	void Update()
     {
@@ -21,16 +28,20 @@ public class PosibleBarquito : MonoBehaviour
 	}
 
 	void Movement()
-	{
-		verticalInput = Input.GetAxis("Vertical");
+    {
+        verticalInput = Input.GetAxis("Vertical");
+		clampedSpeed = Mathf.Clamp(speed, minSpeedLimit, maxSpeedLimit);
 		movementFactor = Mathf.Lerp(movementFactor, verticalInput, Time.deltaTime / movementThresold);
-		transform.Translate(0.0f, 0.0f, movementFactor * speed);
+        transform.Translate(0.0f, 0.0f, movementFactor * clampedSpeed);
+
 	}
 
 	void Steer()
 	{
 		horizontalInput = Input.GetAxis("Horizontal");
+		clampedSteerSpeed = Mathf.Clamp(steerSpeed, minSpeedSteerLimit, maxSpeedSteerLimit);
 		steerFactor = Mathf.Lerp(steerFactor, horizontalInput * verticalInput, Time.deltaTime / movementThresold);
-		transform.Rotate(0.0f, steerFactor * steerSpeed, 0.0f);
+		transform.Rotate(0.0f, steerFactor * clampedSteerSpeed, 0.0f);
+
 	}
 }
