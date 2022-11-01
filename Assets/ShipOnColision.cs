554d4052ed7fc5c3bcebb9cd24dcc5eb88f11c65
CarlosPedretti <carlosluca150@gmail.com>
2022-11-01 16:25:22 -0300

    1/11
    1/11

---
+++ Assets/ShipOnColision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ShipOnColision : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    
+}
--- Assets/Shot.cs
+++ Assets/Shot.cs
@@ -4,30 +4,65 @@ using UnityEngine;
 
 public class Shot : MonoBehaviour
 {
-
+    public GameObject Ship;
     public GameObject bullet;
     public Transform spawnPoint;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    public Transform spawnPoint3;
     public float shotForce = 1500f;
     public float shotRate = 0.5f;
+    public int Municiones = 0;
 
     private float shotRateTime = 0;
 
+
+
+
+    public void OnTriggerEnter(Collider collision)
+    {
+
+         if (collision.gameObject.tag == "Ammo")
+         {
+            Destroy(collision.gameObject);
+            Municiones = Municiones + 1;
+         }
+    }   
+
+
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            if (Time.time > shotRateTime)
+         if (Municiones >= 1)
+         {
+            if (Input.GetKeyDown("space"))
             {
-                GameObject newBullet;
+                Municiones = Municiones - 1;
+                if (Time.time > shotRateTime)
+                {
+                    GameObject newBullet;
 
-                newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+                    newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
 
-                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
+                    newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
 
-                shotRateTime = Time.time + shotRate;
+                    newBullet = Instantiate(bullet, spawnPoint1.position, spawnPoint1.rotation);
 
-                Destroy(newBullet, 1);
+                    newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint1.forward * shotForce);
+
+                    newBullet = Instantiate(bullet, spawnPoint2.position, spawnPoint2.rotation);
+
+                    newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint2.forward * shotForce);
+
+                    newBullet = Instantiate(bullet, spawnPoint3.position, spawnPoint3.rotation);
+
+                    newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint3.forward * shotForce);
+
+                    shotRateTime = Time.time + shotRate;
+
+                    Destroy(newBullet, 1);
+                }
             }
-        }
+
+         }
     }
 }
